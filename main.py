7d9d940c4b4f373c2caf30edea6af77dc956b8cc
Jesus Daniel Quiroga Saldaña <jdquiroga2410@gmail.com>
2022-10-21 20:05:44 -0500

    add ip global ip

--- main.py
+++ main.py
@@ -34,6 +34,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
